f5ed0f464303dcb91f62e5a8d9beed9c7b1fc9f0
d2jvkpn <d2jvkpn@noreplay.local>
2024-10-09 23:12:10 +0800

    ...

--- deployments/docker_build.sh
+++ deployments/docker_build.sh
@@ -111,7 +111,7 @@ docker build --no-cache --file ${_path}/Dockerfile \
 #### 5. push image
 [ "$DOCKER_Push" != "false" ] && docker push $image
 
-#### 6.
+#### 6. remove dangling images
 docker image prune --force --filter label=stage=${app_name}_builder &> /dev/null
 # docker images --filter "dangling=true" --quiet $image | xargs -i docker rmi {}
 for img in $(docker images -f "dangling=true" -f label=app=${app_name} --quiet); do
