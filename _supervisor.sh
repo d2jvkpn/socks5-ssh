5b1d9861f80f31268deb366c46ea436f94793601
d2jvkpn <d2jvkpn@noreplay.local>
2024-11-07 16:50:03 +0800

    ...

--- _supervisor.sh
+++ _supervisor.sh
@@ -13,6 +13,8 @@ curl -x sock5h://127.0.0.1:1090 https://icanhazip.com
 
 # docker exec -it socks5-openvpn bash
 
+# docker exec -it socks5-openvpn ssh -F configs/ssh.conf remote_host
+
 exit
 cat > configs/supervisord.conf <<EOF
 # path: /etc/supervisord.conf
